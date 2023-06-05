dacfea5e1eba910c9870bf7e9c7281db011f8265
dvarun05 <134685168+dvarun05@users.noreply.github.com>
2023-06-05 22:11:38 +0530

    adjusting

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var data1={image : "photo 1.jpeg",name : "DECHU VARUN",designation : "Student"};
 var data2={image : "photo 4.jpeg",name : "VARUN",designation : "Software Developer"};
 var isdata1 =true;
-var dispalydata;
+var displaydata;
 var changingpic=function(){
    if(isdata1)
     {
-        dispalydata=data2;
+        displaydata=data2;
         isdata1=false;
     }
     else{
-        dispalydata=data1;
+        displaydata=data1;
         isdata1=true;
     }
     document.getElementById("myimage").src=displaydata.image;
-    document.getElementById("title").innerHTML=dispalydata.name;
-    document.getElementById("mydesignation").innerHTML=dispalydata.designation;
+    document.getElementById("title").innerHTML=displaydata.name;
+    document.getElementById("mydesignation").innerHTML=displaydata.designation;
     
 
 }
