596ef07cc8bd6a0c64c4220380bf2992a9506888
dvarun05 <134685168+dvarun05@users.noreply.github.com>
2023-06-05 21:40:49 +0530

    perform first DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the Page")
+var data1={image : "photo1.jpeg",name : "DECHU VARUN",designation : "Student"};
+var data2={image : "photo4.jpeg",name : "VARUN",designation : "Software Developer"};
+var isdata1 =true;
+var dispalydata;
+var changingpic=function(){
+   if(isdata1)
+    {
+        dispalydata=data2;
+        isdata1=false;
+    }
+    else{
+        dispalydata=data1;
+        isdata1=true;
+    }
+    document.getElementById("title").src=displaydata.image;
+}
